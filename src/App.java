7ad063451f93f222fcb83a955eb058abfe2f6ac2
Jessica0000000 <jessicaflower12@outlook.com>
2024-12-10 09:20:02 -0500

    done

--- src/App.java
+++ src/App.java
@@ -5,6 +5,8 @@ public class App extends PApplet {
 
     ArrayList<Circle> circles;
     double timer;
+    int scene = 0;
+    int attackStat = 0;
 
     public static void main(String[] args) {
         PApplet.main("App");
@@ -14,33 +16,55 @@ public class App extends PApplet {
     public void setup() {
         circles = new ArrayList<>();
         int numCircles = 0;
-        while (numCircles < 100) {
-            int x = (int) random(700);
-            int y = (int) random(400);
+        while (numCircles < 60) {
+            int x = (int) random(30, 660);
+            int y = (int) random(40, 560);
             Circle c = new Circle(x, y, this);
             circles.add(c);
             numCircles++;
-           
+
         }
 
     }
 
     public void settings() {
-        size(900, 600);
+        size(700, 600);
 
     }
 
     public void draw() {
         background(0);
 
-        for (Circle d : circles) {
-            d.display();
+        if (scene == 0) {
+            for (Circle d : circles) {
+                d.display();
+            } // make the circle appear on screen
+
+            fill(255);
+            textSize(25);
+            timer = 20 - millis() / 1000;
+            text("Time: " + timer, width - 150, 50);
+            if (timer == 0) {
+                scene++;
+                sceneOne();
+            } // changing the background and transitioning to the next part of the game
         }
+    }
 
-        fill(255);
-        textSize(25);
-        timer = millis();
-        text("Time: " + timer*.001*1.00,width - 150, 50);
+    public void sceneOne() {
 
+            attackStat = circles.size();
+
+            System.out.println(attackStat);
+         // calculate the strength of your attack
+    }
+
+    public void mousePressed() {
+        for (int i = 0; i < circles.size(); i++) {
+            Circle c = circles.get(i);
+            if (c.checkIfMouseTouch(mouseX, mouseY) == true) {
+                circles.remove(c);
+            } // removing circles when clicked
+        }
     }
 }
--- src/Circle.java
+++ src/Circle.java
@@ -9,18 +9,34 @@ public class Circle {
     private int y;
     private PApplet canvas;
 
-    public Circle(int xPos, int yPos, PApplet c){
+    public Circle(int xPos, int yPos, PApplet c) {
         x = xPos;
         y = yPos;
-        size = 60;
+        size = 80;
         canvas = c;
-        color = (255);
+        color = randomColor();
     }
-    public void display(){
+
+    public void display() {
         canvas.fill(color);
-        canvas.circle(x,y,size);
+        canvas.circle(x, y, size);
     }
+
+    public int randomColor() {
+        return canvas.color(canvas.random(255), canvas.random(255), canvas.random(255));
+    }
+
+    public boolean checkIfMouseTouch(int mouseX, int mouseY) {
+        float distanceFromCenter = canvas.dist(x, y, mouseX, mouseY);
+        if (distanceFromCenter < size / 2) {
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
     public void checkNumCircles(){
-        if ()
+        for(Circle d: circles)
     }
 }
