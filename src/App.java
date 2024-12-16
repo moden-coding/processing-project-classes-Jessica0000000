0604da1bc1ce39ab20db65c68912be9d71449eff
Jessica0000000 <jessicaflower12@outlook.com>
2024-12-16 11:07:29 -0500

    done

--- src/App.java
+++ src/App.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class App extends PApplet {
 
-    ArrayList<Circle> circles;//creates array list for circles
-    ArrayList<Sword> swords;//creates array list for swords
+    ArrayList<Circle> circles;// creates array list for circles
+    ArrayList<Sword> swords;// creates array list for swords
     double timer;
     int scene = 0;
     int attackStat = 0;
@@ -26,13 +26,6 @@ public class App extends PApplet {
             attackStat++;// create attack stat equal to the number of circles spawning
 
         } // creates 60 circles
-        swords= new ArrayList<>();
-       while(attackStat>0){
-        int xTwo = (int) random(30, 660);
-        int yTwo= (int) random(40, 560);
-        Sword s = new Sword(xTwo, yTwo, this);
-        swords.add(s);
-       }
 
     }
 
@@ -48,30 +41,37 @@ public class App extends PApplet {
             for (Circle d : circles) {
                 d.display();
             } // make the circle appear on screen
-            //make sword appear
-            for (Sword s : swords) {
-                s.displaySword();
-            }
 
             fill(255);
             textSize(25);
-            timer = 10 - millis() / 1000;
+            timer = 1 - millis() / 1000;
             text("Time: " + timer, width - 150, 50);// creating the timer counting down from 10 in the first part of the
 
-            // game
             if (timer == 0) {
                 scene++;
                 sceneOne();// changing the background and transitioning to the next part of the game
             }
+        }
+        if (scene == 1) {
+            for (Sword s : swords) {
+                s.displaySword();
+            } // when the scene changes make the swords appear
         }
     }
 
     public void sceneOne() {
         background(0, 35, 255);// changing the background for the next part of the game
 
+        swords = new ArrayList<>();// creating new array list for swords
+        while (attackStat > 0) {
+            int x = (int) random(0, 650);
+            int y = (int) random(400, 250);
+            int yTwo = 200;
+            Sword s = new Sword(x, y, yTwo, this);
+            swords.add(s);
+            attackStat--;
+        } // creates number of swords equal to the attackStat
         System.out.println(attackStat);
-
-        line(200, 200, 200, 400);
 
     }
 
@@ -88,45 +88,3 @@ public class App extends PApplet {
         // boss caterpillarBoss = new boss(attackStat/2);
     }
 }
-
-//etra code for sword:
- // Sword blade
-//  fill(220);
-//  stroke(0);
-//  strokeWeight(2);
-//  beginShape();
-//  vertex(195, 200); // Top point
-//  vertex(205, 200);
-//  vertex(210, 300);
-//  vertex(210, 400);
-//  vertex(190, 400);
-//  vertex(190, 300);
-//  endShape(CLOSE);
-
-//  // Blade outline
-//  line(200, 200, 200, 400);
-
-//  // Sword guard
-//  fill(150);
-//  noStroke();
-//  arc(200, 410, 40, 20, PI, TWO_PI);
-
-//  // Guard details
-//  stroke(0);
-//  strokeWeight(1);
-//  line(180, 410, 220, 410);
-
-//  // Handle grip
-//  fill(139, 69, 19);
-//  rect(195, 420, 10, 50);
-
-//  // Handle wrapping details
-//  stroke(0);
-//  for (int i = 0; i < 5; i++) {
-//      line(195, 420 + i * 10, 205, 425 + i * 10);
-//      line(205, 420 + i * 10, 195, 425 + i * 10);
-//  }
-
-//  // Pommel
-//  fill(200);
-//  ellipse(200, 475, 15, 15);
--- src/Sword.java
+++ src/Sword.java
@@ -5,21 +5,29 @@ public class Sword {
     private int size;
     private int x;
     private int y;
+    private int yTwo;
     private PApplet canvas;
+    private int color;
 
-    public Sword(int attackStat, int xPos, int yPos, PApplet c) {
-        x = xPos;
-        y = yPos;
-        size = 40;
+    public Sword(int xPos, int yPos, int yPost, PApplet c) {
+        x = xPos+ 40;
+        y = yPos+50;
+        yTwo= yPost+400;
+        size = 4;
         canvas = c;
+        color=randomColor();
     }
     public void displaySword() {
-        canvas.fill(0);
-        canvas.line(x, y);//add another y value and add strokeweight with strokeweight(1) after I create the line
+        canvas.fill(color);
+        canvas.strokeWeight(size);
+        canvas.line(x, y,x,yTwo);//add another y value and add strokeweight with strokeweight(1) after I create the line
+   System.out.println(color);//right now color isn't working, it's constantly changing and the swords look black
     }
 
     // public void increaseAttack(){
     // attack += 5;
     // }
-
+    public int randomColor() {
+        return canvas.color(canvas.random(255), canvas.random(255), canvas.random(255));
+    }
 }
